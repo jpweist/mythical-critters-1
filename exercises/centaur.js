47af1a2394bed5031957c7fad3a3d93052dfaa77
Jim Weist <jp_weist@yahoo.com>
2019-09-02 17:26:31 -0600

    Part way thru centaur error on test file line 51

---
+++ exercises/centaur.js
@@ -0,0 +1,45 @@
+class Centaur {
+  constructor (name) {
+    this.name = name;
+    this.breed = 'Palomino';
+    this.cranky = false;
+    this.standing = true;
+    this.runShoot = 0;
+    this.layingDown = false;
+  }
+  shoot() {
+    if (this.standing === true && this.layingDown === false && this.runShoot < 3) {
+      this.runShoot++;
+        return 'Twang!!!';
+      } else {
+        this.cranky = true;
+        return 'NO!';
+    }
+
+
+  }
+  run() {
+    if (this.standing === true && this.layingDown === false && this.runShoot < 3) {
+      this.runShoot++;
+        return 'Clop clop clop clop!!!';
+      } else {
+        this.cranky = true;
+        return 'NO!';
+    }
+  }
+
+
+  sleep() {
+    return 'NO!';
+  }
+  layDown() {
+    this.standing = false;
+    this.layingDown = true;
+  }
+  standUp() {
+    this.standing = true;
+    this.layingDown = false;
+  }
+}
+
+module.exports = Centaur;
